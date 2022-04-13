41ae2a55908c487ef713fbf2993ceb9427fe0a3e
msyahrulsp <mssp892@gmail.com>
2022-04-13 10:51:05 +0700

    add soal 1, 5, 6

--- util.py
+++
@@ -1,9 +0,0 @@
-class bcolors:
-    GREY = '\33[90m'
-    RED = '\33[91m'
-    GREEN = '\33[92m'
-    YELLOW = '\33[93m'
-    BLUE = '\33[94m'
-    VIOLET = '\33[95m'
-    BEIGE = '\33[96m'
-    WHITE = '\33[97m'
